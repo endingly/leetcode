60363b803579139c6b297ddb62389b74cd1e30f1
endingly <jiasongdeng@qq.com>
2024-01-18 10:17:15 +0800

    2744

--- leetcode/maximumNumberOfStringPairs.cpp
+++ leetcode/maximumNumberOfStringPairs.cpp
@@ -1,8 +1,32 @@
 #include <gtest/gtest.h>
+
 #include <unordered_set>
 using std::vector, std::string;
 
-int maximumNumberOfStringPairs(vector<string>& words) {
-    std::unordered_set<string> tmp;
-     
+int Parse(const std::string& str) {
+  auto a = str[0] * 100;
+  auto b = str[1];
+  return a + b;
 }
+
+int ParseReverse(const std::string& str) {
+  auto a = str[1] * 100;
+  auto b = str[0];
+  return a + b;
+}
+
+int maximumNumberOfStringPairs(vector<string>& words) {
+  std::unordered_set<int> tmp;
+  int                     sum = 0;
+  for (auto&& i : words) {
+    auto num = ParseReverse(i);
+    if (tmp.find(num) != tmp.end()) {
+      sum++;
+      tmp.erase(num);
+    } else {
+      tmp.insert(Parse(i));
+    }
+  }
+  return sum;
+}
+
